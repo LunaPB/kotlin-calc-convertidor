3989fd7884196ed9b1fea4a73e367f797132dd81
Brian Luna <lunapadronbrian@gmail.com>
2026-05-25 20:32:11 -0500

    Menú secundario y conversiones numéricas para convertidor de unidades

--- src/Main.kt
+++ src/Main.kt
@@ -25,7 +25,7 @@ fun main() {
             // Mandamos a llamar a la función de cada programa
             1 -> ejecutarCalculadoraIMC()
 
-            2 -> println("\n[ Iniciando Convertidor de Unidades... ]")
+            2 -> ejecutarConvertidor()
 
             3 -> {
                 println("Saliendo del programa. ¡Hasta luego!")
@@ -60,5 +60,37 @@ fun ejecutarCalculadoraIMC() {
         }
     } else {
         println("Error: Valores inválidos. Asegúrate de usar números positivos.")
+    }
+}
+fun ejecutarConvertidor() {
+    println("\n[ Iniciando Convertidor de Unidades ]")
+    println("1. Celsius a Fahrenheit")
+    println("2. Metros a Pies")
+    print("Selecciona una conversión: ")
+
+    val opcion = readlnOrNull()?.toIntOrNull() ?: 0
+
+    when (opcion) {
+        1 -> {
+            print("Ingresa los grados Celsius: ")
+            val celsius = readlnOrNull()?.toDoubleOrNull()
+            if (celsius != null) {
+                val fahrenheit = (celsius * 9/5) + 32
+                println("$celsius °C equivalen a ${String.format("%.2f", fahrenheit)} °F")
+            } else {
+                println("Entrada inválida.")
+            }
+        }
+        2 -> {
+            print("Ingresa la longitud en metros: ")
+            val metros = readlnOrNull()?.toDoubleOrNull()
+            if (metros != null) {
+                val pies = metros * 3.28084
+                println("$metros metros equivalen a ${String.format("%.2f", pies)} pies")
+            } else {
+                println("Entrada inválida.")
+            }
+        }
+        else -> println("Opción de conversión no válida.")
     }
 }
