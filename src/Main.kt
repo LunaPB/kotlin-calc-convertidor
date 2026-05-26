f85f98cefb1578c1be2e4141b407289fcd6eea6f
Brian Luna <lunapadronbrian@gmail.com>
2026-05-25 20:23:35 -0500

    Implementación de lógica matemática y clasificación para la Calculadora de IMC

--- src/Main.kt
+++ src/Main.kt
@@ -22,13 +22,43 @@ fun main() {
         val opcion = readlnOrNull()?.toIntOrNull() ?: 0
 
         when (opcion) {
-            1 -> println("\n[ Iniciando Calculadora de IMC... ]")
+            // Mandamos a llamar a la función de cada programa
+            1 -> ejecutarCalculadoraIMC()
+
             2 -> println("\n[ Iniciando Convertidor de Unidades... ]")
+
             3 -> {
                 println("Saliendo del programa. ¡Hasta luego!")
                 ejecutando = false
             }
             else -> println("Opción no válida. Intenta de nuevo.")
         }
+    }
+}
+fun ejecutarCalculadoraIMC() {
+    println("\n[ Iniciando Calculadora de IMC ]")
+    print("Ingresa tu peso en kilogramos (ej. 70.5): ")
+    val peso = readlnOrNull()?.toDoubleOrNull()
+
+    print("Ingresa tu altura en metros (ej. 1.75): ")
+    val altura = readlnOrNull()?.toDoubleOrNull()
+
+    // Verificación de variables: que no sean nulas y la altura sea válida
+    if (peso != null && altura != null && altura > 0.0) {
+        val imc = peso / (altura * altura)
+        // Redondeamos el resultado numérico a 2 decimales
+        val imcFormateado = String.format("%.2f", imc)
+
+        print("Tu IMC es $imcFormateado. Clasificación: ")
+
+        // Uso de when sin argumento para evaluar rangos lógicos
+        when {
+            imc < 18.5 -> println("Bajo peso")
+            imc in 18.5..24.9 -> println("Peso normal")
+            imc in 25.0..29.9 -> println("Sobrepeso")
+            else -> println("Obesidad")
+        }
+    } else {
+        println("Error: Valores inválidos. Asegúrate de usar números positivos.")
     }
 }
